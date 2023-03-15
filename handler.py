cb2d804ffacef6ab179c625856511aeb13faa4e6
RohanVDvivedi <rohandvivedi@gmail.com>
2023-03-15 15:38:18 -0700

    added comment consisting fo pseudocode

--- handler.py
+++ handler.py
@@ -12,5 +12,19 @@ def open_encoding(filename):
 	file.close()
 	return data
 
-def face_recognition_handler(event, context):	
-	print("Hello")
+def face_recognition_handler(event, context):
+	# upload images of all known people into face_recognition library
+	
+	# extract key of the file pushed
+	key = event['Records'][0]['s3']['object']['key']
+	print("Hello + " + key)
+
+	# grab video file from the bucket using the key
+
+	# loop over the frames of the ffmpeg extracting each frames
+
+		# pass the frame to face_recognition library to extract names and location of all people identitied
+
+		# pass the list of names to abhishek's function
+
+		# if abhishek's function returns true, quit the loop
