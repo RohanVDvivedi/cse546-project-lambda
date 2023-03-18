1b0e76846394ace16b4db39b7b92721903d88f99
RohanVDvivedi <rohandvivedi@gmail.com>
2023-03-18 16:41:17 -0700

    bug fix file permissions

--- handler.py
+++ handler.py
@@ -11,11 +11,8 @@ import os
 import shutil
 import pathlib
 
-input_bucket = "lambda-input-088664932081"
-output_bucket = "lambda-outputbucket-aqls5g3tm1f5"
 dynamoDB_table_name = "students"
-
-input_bucket = "lambda-input-088664932081"
+input_bucket = "inputbucket-088664932081"
 output_bucket = "lambda-output-cse546"
 
 aws_session = boto3.Session(
@@ -37,12 +34,12 @@ def face_recognition_handler(event, context):
 
 	# save the video file to cwd/temp
 	s3_client = aws_session.client('s3')
-	if not os.path.exists(os.getcwd() + "/temp"):
-		os.makedirs(os.getcwd() + "/temp")
+	if not os.path.exists("/tmp/temp"):
+		os.makedirs("/tmp/temp")
 	s3_client.download_file(input_bucket, key, os.getcwd() + "/temp")
 
 	# convert the video file to set of frames
-	os.system("ffmpeg -i " + os.getcwd() + "/temp/" + key + " -r 1 " + os.getcwd() + "/temp/image-%3d.jpeg")
+	os.system("ffmpeg -i /tmp/temp/" + key + " -r 1 /tmp/temp/image-%3d.jpeg")
 
 	# load images of all known people into face_recognition library
 	model = open_encoding("/home/app/encoding")
@@ -51,7 +48,7 @@ def face_recognition_handler(event, context):
 
 	# loop over the frames of the ffmpeg extracting each frames
 	done = False
-	for frame_path in list(pathlib.Path(os.getcwd() + "/temp").glob('*.jpeg')) :
+	for frame_path in list(pathlib.Path("/tmp/temp").glob('*.jpeg')) :
 		frame = face_recognition.load_image_file(frame_path)
 		frame_encoding = face_recognition.face_encodings(frame)[0]
 		results = face_recognition.compare_faces(model_encoding, frame_encoding)
@@ -66,7 +63,7 @@ def face_recognition_handler(event, context):
 			break
 	
 	# delete all frames and images
-	shutil.rmtree(os.getcwd() + "/temp")
+	shutil.rmtree("/tmp/temp")
 
 def upload_csv_to_s3(csv_file_path, bucket_name, object_name):
     # Create an S3 client
