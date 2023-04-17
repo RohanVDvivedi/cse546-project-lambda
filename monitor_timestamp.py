ec40d97d754a85e9812a54c1d55efa9451c1521b
RohanVDvivedi <rohandvivedi@gmail.com>
2023-04-17 14:23:32 -0700

    added final script for the openstack vm

---
+++ monitor_timestamp.py
@@ -0,0 +1,34 @@
+import os
+import boto3
+from datetime import datetime
+import time
+
+aws_session = boto3.Session(
+    aws_access_key_id="AKIARJJGXP3YRNZOPD72",
+    aws_secret_access_key="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",
+    region_name="us-east-1"
+)
+
+s3_client = aws_session.client('s3')
+lambda_client = aws_session.client('lambda')
+
+def main():
+    tPrev = None
+    while True:
+        response = s3_client.list_objects_v2(Bucket='inputbucketproject3new')
+        if 'Contents' in response:
+            objectKeys = response['Contents']
+            # filter out objects with last modified time greater than or equal to tPrev
+            objectKeys = [obj for obj in objectKeys if (tPrev == None or obj['LastModified'] > tPrev)]
+            if len(objectKeys) == 0:
+                time.sleep(2)  # Wait for 2 seconds before checking again
+                continue
+            # sort the objects by last modified time in descending order, latest last_modified will be first
+            objectKeys = sorted(objectKeys, key=lambda obj: obj['LastModified'], reverse=True)
+            tPrev = objectKeys[0]['LastModified']
+            for objKey in objectKeys:
+                # Invoke the Lambda function with the object key as input
+                lambda_client.invoke(FunctionName='lambdaforproject3', Payload='{"key":"' + objKey['Key'] + '"}')
+
+if __name__ == '__main__':
+    main()
